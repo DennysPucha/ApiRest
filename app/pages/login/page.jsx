347db7940597a2e030934cdeb89ea6a36fad999e
DennysPucha <dennys.pucha@unl.edu.ec>
2024-07-31 17:59:06 -0500

    debugging

--- app/pages/login/page.jsx
+++ app/pages/login/page.jsx
@@ -1,11 +1,11 @@
 "use client";
-import {Input} from '../../../components/forms/Forms';
+import { Input } from '../../../components/forms/Forms';
 import Link from 'next/link';
 import * as yup from 'yup';
-import {useForm} from 'react-hook-form';
-import {yupResolver} from '@hookform/resolvers/yup';
-import {LoginConst} from '../../../components/forms/Functions';
-import { Toaster} from 'sonner'
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { LoginConst } from '../../../components/forms/Functions';
+import { Toaster } from 'sonner'
 
 export default function Page() {
 
@@ -14,7 +14,7 @@ export default function Page() {
         password: yup.string().required("El campo de contraseña es requerido")
     });
 
-    const {register, handleSubmit, formState: {errors}} = useForm({ resolver: yupResolver(schema) });
+    const { register, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(schema) });
 
     const onSubmit = async (data) => await LoginConst(data)
 
@@ -22,9 +22,6 @@ export default function Page() {
         <div>
             <Toaster />
             <div className="bg-gray-100 flex justify-center items-center h-screen">
-                <div className="w-1/2 h-screen hidden lg:block">
-                    <img src="https://placehold.co/800x/667fff/ffffff.png?text=Your+Image&font=Montserrat" alt="Placeholder Image" className="object-cover w-full h-full"></img>
-                </div>
                 <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
                     <h1 className="text-2xl font-semibold text-gray-600 mb-4">Inicio de sesión</h1>
                     <form onSubmit={handleSubmit(onSubmit)}>
@@ -33,13 +30,13 @@ export default function Page() {
                             <Input {...register("email")} type="text" id="email" />
                             <a className="text-red-500">{errors.email?.message}</a>
                         </div>
-                        
+
                         <div className="mb-4">
                             <label className="block text-gray-600">Contraseña</label>
                             <Input {...register("password")} type="password" id="password" />
                             <a className="text-red-500">{errors.password?.message}</a>
                         </div>
-                        
+
                         <div className="mb-6 text-blue-500">
                             <Link href="#" className="hover:underline">Forgot Password?</Link>
                         </div>
